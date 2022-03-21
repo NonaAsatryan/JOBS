f26b1190fa4b78994900cbbe01d26b37b28a613a
ani-gasparyan <ani-gasparyan-2021@mail.ru>
2022-03-21 20:34:03 +0400

    Create entity, repository and type for Job.

---
+++ src/main/java/com/example/jobs/entity/Job.java
@@ -0,0 +1,37 @@
+package com.example.jobs.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "job")
+public class Job {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private JobType jobType;
+    private double salary;
+
+    @ManyToOne
+    private Company company;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Category category;
+
+}
