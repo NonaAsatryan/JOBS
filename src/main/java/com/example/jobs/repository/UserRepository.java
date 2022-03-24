f3d17e8d7630ab881f2d449dcaeb6cef8fafdcd8
nona.asatryan <nonadev@gmail.com>
2022-03-24 18:21:21 +0400

    change UserRepository

--- src/main/java/com/example/jobs/repository/UserRepository.java
+++ src/main/java/com/example/jobs/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.example.jobs.repository;
 import com.example.jobs.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 
