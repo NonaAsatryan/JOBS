acccd467ccc508e4321b8a49ecee8a142433f9e5
ani-gasparyan <ani-gasparyan-2021@mail.ru>
2022-04-04 17:33:45 +0400

    Change IndexController and applied-jo.html, bookmark.html,delete-account.html, details.html.

--- src/main/java/com/example/jobs/controller/IndexController.java
+++ src/main/java/com/example/jobs/controller/IndexController.java
@@ -1,14 +1,95 @@
 package com.example.jobs.controller;
 
+import com.example.jobs.entity.Category;
+import com.example.jobs.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final CategoryService categoryService;
+
+
     @GetMapping("/")
-    public String main(ModelMap map) {
+    public String main() {
         return "index";
+    }
+
+    @GetMapping("/job/post")
+    public String postPage(ModelMap map) {
+        List<Category> categoryList = categoryService.findAll();
+        map.addAttribute("categoryList", categoryList);
+        return "post";
+    }
+
+    @GetMapping("/resume")
+    public String resumePage() {
+        return "resume";
+    }
+
+    @GetMapping("/user/register")
+    public String registerPage() {
+        return "signup";
+    }
+
+    @GetMapping("/user/signin")
+    public String signinPage() {
+        return "signin";
+    }
+
+    @GetMapping("/job/details")
+    public String detailsPage() {
+        return "details";
+    }
+
+    @GetMapping("/job/jobList")
+    public String jobListPage() {
+        return "job-list";
+    }
+
+    @GetMapping("/job/jobDetails")
+    public String jobDetailsPage() {
+        return "job-details";
+    }
+
+    @GetMapping("/resume/post")
+    public String postResumePage() {
+        return "post-resume";
+    }
+
+    @GetMapping("/user/profile")
+    public String userProfilePage() {
+        return "profile";
+    }
+
+    @GetMapping("/resume/edit")
+    public String editResumePage() {
+        return "edit-resume";
+    }
+
+    @GetMapping("/user/profileDetails")
+    public String profileDetailsPage() {
+        return "profileDetails";
+    }
+
+    @GetMapping("/bookmark")
+    public String bookmarkPage() {
+        return "bookmark";
+    }
+
+    @GetMapping("/job/appliedJob")
+    public String appliedJobPage() {
+        return "applied-job";
+    }
+
+    @GetMapping("/user/deleteAccount")
+    public String deleteAccountPage() {
+        return "delete-account";
     }
 }
--- src/main/java/com/example/jobs/controller/JobController.java
+++ src/main/java/com/example/jobs/controller/JobController.java
@@ -3,10 +3,18 @@ package com.example.jobs.controller;
 import com.example.jobs.service.JobService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class JobController {
 
     private final JobService jobService;
+
+    @PostMapping("/job/post")
+    public String postJob(ModelMap map) {
+        return "post";
+    }
+
 }
