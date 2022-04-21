11280f66fd2eba553ea31639f90cb695ec90939b
ani-gasparyan <ani-gasparyan-2021@mail.ru>
2022-04-21 19:32:38 +0400

    Changes .

--- src/main/java/com/example/jobs/dto/CreateJobRequest.java
+++ src/main/java/com/example/jobs/dto/CreateJobRequest.java
@@ -1,17 +1,14 @@
 package com.example.jobs.dto;
 
 
-import com.example.jobs.entity.Category;
-import com.example.jobs.entity.Company;
 import com.example.jobs.entity.JobType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -20,6 +17,7 @@ public class CreateJobRequest {
 
 
     private int id;
+    @NotEmpty(message = "Name can't be empty")
     private String name;
     private int categoryId;
     private String description;
